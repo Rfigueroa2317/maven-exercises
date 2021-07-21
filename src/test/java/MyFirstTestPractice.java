da56a740ee9d499ce8d9878e2c33e8b0c84fdcc7
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-21 10:42:31 -0500

    update

---
+++ src/test/java/MyFirstTestPractice.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTestPractice {
+
+    // Create a test method that uses assertEquals or assertNotEquals
+    // to verify if the String Codeup is the same as CodeUp.
+
+    @Test
+    public void assertEquals() {
+        String expected = "Codeup";
+        String actual = "CodeUp";
+    }
+
+    // Create a test method that uses assertNotSame to verify that the
+    // following ArrayLists are not the same:
+
+    @Test
+    public void assertNotSame() {
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+    }
+
+    // Create a test method that uses assertArrayEquals to verify the
+    // following arrays are equal.
+
+    @Test
+    public void assertArrayEquals() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+    }
+
+
+}
