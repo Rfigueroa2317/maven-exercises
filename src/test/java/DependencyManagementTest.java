bcf4bb4cdd1055a201fb9f430835914952a99d2e
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-21 11:21:30 -0500

    update

---
+++ src/test/java/DependencyManagementTest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.String;
+
+public class DependencyManagementTest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter some text here: ");
+        String message = scanner.nextLine();
+
+    }
+
+
+}
--- src/test/java/MyFirstTestPractice.java
+++ src/test/java/MyFirstTestPractice.java
@@ -11,32 +11,32 @@ public class MyFirstTestPractice {
     // Create a test method that uses assertEquals or assertNotEquals
     // to verify if the String Codeup is the same as CodeUp.
 
-    @Test
-    public void assertEquals() {
-        String expected = "Codeup";
-        String actual = "CodeUp";
-    }
+//    @Test
+//    public void assertEquals() {
+//        String expected = "Codeup";
+//        String actual = "CodeUp";
+//    }
 
     // Create a test method that uses assertNotSame to verify that the
     // following ArrayLists are not the same:
 
-    @Test
-    public void assertNotSame() {
-        List<String> languages = new ArrayList<>();
-        List<String> moreLanguages = new ArrayList<>();
-    }
+//    @Test
+//    public void assertNotSame() {
+//        List<String> languages = new ArrayList<>();
+//        List<String> moreLanguages = new ArrayList<>();
+//    }
 
     // Create a test method that uses assertArrayEquals to verify the
     // following arrays are equal.
 
-    @Test
-    public void assertArrayEquals() {
-        int[] numbers = {1, 2, 3};
-        int[] otherNumbers = new int[3];
-        otherNumbers[0] = 1;
-        otherNumbers[1] = 2;
-        otherNumbers[2] = 3;
-    }
+//    @Test
+//    public void assertArrayEquals() {
+//        int[] numbers = {1, 2, 3};
+//        int[] otherNumbers = new int[3];
+//        otherNumbers[0] = 1;
+//        otherNumbers[1] = 2;
+//        otherNumbers[2] = 3;
+//    }
 
 
 }
